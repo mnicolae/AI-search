7ceb2a01a8be196ebe353bdd2fe73970df1edffd
Mihai Nicolae <m.nicolae91@gmail.com>
2015-02-05 11:54:37 -0500

    commit so I can work on mac

--- bicycle.py
+++ bicycle.py
@@ -14,7 +14,7 @@ from random import randint
 from math import sqrt
 
 class bicycle(StateSpace):
-    def __init__(self, action, gval, state, parent = None, map, job_list):
+    def __init__(self, action, gval, state, parent = None, map, job_list, max_weight=10000):
 #IMPLEMENT
         '''Initialize a bicycle search state object.'''
         if action == 'START':   #NOTE action = 'START' is treated as starting the search space
@@ -23,15 +23,39 @@ class bicycle(StateSpace):
         #implement the rest of this function.
         self.map = map
         self.job_list = job_list
-
+        self.max_weight = max_weight
+        
     def successors(self): 
 #IMPLEMENT
         '''Return list of bicycle objects that are the successors of the current object'''
-
+        States = list()
+        
+        # Location is home, can only perform first_pickup actions
+        if get_loc(self) == 'home':
+            for job_name in get_unstarted(self):
+                job_info = get_job_info(job_list, job_name)
+                
+                job_pickup_location = job_info[1]
+                job_pickup_time = job_info[2]
+                job_weight = job_info[4]
+                travel_time = 0 # TODO: initialize the travel time
+                if job_weight + get_load(self) <= self.max_weight:
+                    #new_state = {"jobs_carried": list(get_carrying(self)),
+                    #             "jobs_carried_weight": list(get_load(self),
+                    #             "location": job_pickup_location,
+                    #             "time": max(get_time(self) + travel_time, job_pickup_time),
+                    #             "earnings": get_earned(self),
+                    #             "jobs_not_started": jobs_not_started}
+                    
+        # Location is not home, can only perform pickup or deliver actions
+        else:
+            # TODO
+        return States
+    
     def hashable_state(self) :
 #IMPLEMENT
         '''Return a data item that can be used as a dictionary key to UNIQUELY represent the state.'''
-        
+        return ()
     def print_state(self):
         #DO NOT CHANGE THIS FUNCTION---it will be used in auto marking
         #and in generating sample trace output. 
@@ -50,33 +74,42 @@ class bicycle(StateSpace):
                       self.get_time(), self.get_loc(), self.get_earned()))
         print("    Unstarted Jobs.{}".format(self.get_unstarted()))
 
+    def get_job_info(job_name, job_list):
+        ''' Given a job name return its corresponding entry in job_list. 
+            Assume there always exists a corresponding entry. '''
+        for index in range(len(job_list)):
+            if job_list[index][0] == job_name:
+                return job_list[index]
+            
     def get_loc(self):
 #IMPLEMENT
         '''Return location of courier in this state'''
-
+        return self.state['location']
+    
     def get_carrying(self):
 #IMPLEMENT
         '''Return list of NAMES of jobs being carried in this state'''
+        return self.state['jobs_carried']
     
     def get_load(self):
 #IMPLEMENT
         '''Return total weight being carried in this state'''
-
+        return self.state['jobs_carried_weight']
+    
     def get_time(self):
 #IMPLEMENT
         '''Return current time in this state'''
-
-    def get_loc(self):
-#IMPLEMENT
-        '''Return current location in this state'''
-
+        return self.state['time']
+    
     def get_earned(self):
 #IMPLEMENT
         '''Return amount earned so far in this state'''
-
+        return self.state['earnings']
+    
     def get_unstarted(self):
 #IMPLEMENT
         '''Return list of NAMES of jobs not yet stated in this state''' 
+        return self.state['jobs_not_started']
     
 def heur_null(state):
     '''Null Heuristic use to make A* search perform uniform cost search'''
@@ -115,6 +148,7 @@ def make_start_state(map, job_list):
         jobs_not_started.append(job[0]);
     
     state = {"jobs_carried": [],
+             "jobs_carried_weight": 0,
              "location": "home",
              "time": 420,
              "earnings": 0,
@@ -196,3 +230,9 @@ def test(nloc, njobs):
     final = se.search(s0, bicycle_goal_fn, heur_max_delivery_costs)
     
 
+### TESTING AREA
+if __name__ == '__main__':
+    job_list = [['Job1', 'locA', 480, 'locC', 5000, [[510, 25], [570, 20], [600, 10], [660, 5]]],
+                ['Job2', 'locB', 600, 'locC', 5000, [[630, 25], [730, 5]]],
+                ['Job3', 'locC', 540, 'locD', 10000, [[545, 50], [570, 25], [600, 5]]]]
+    return 0
